0148bc9a3db3052a82784845ca24f8509b75db5e
santiolsze <solszevicki@gmail.com>
2022-09-27 14:59:46 -0300

    new_exp

--- c2/z723_propia.r
+++ c2/z723_propia.r
@@ -43,7 +43,7 @@ hs <- makeParamSet(
 #  muy pronto esto se leera desde un archivo formato .yaml
 PARAM  <- list()
 
-PARAM$experimento  <- "HT7231"
+PARAM$experimento  <- "HT7232"
 
 PARAM$input$dataset       <- "./datasets/competencia2_2022.csv.gz"
 PARAM$input$training      <- c( 202103 )
