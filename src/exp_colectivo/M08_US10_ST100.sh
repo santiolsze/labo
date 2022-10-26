0a02ff6ddc2c1709e5a6b6a56dff522e222183b3
santiolsze <solszevicki@gmail.com>
2022-10-26 11:11:33 -0300

    M8US10ST100

---
+++ src/exp_colectivo/M08_US10_ST100.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+nohup Rscript 3_training_strategy_under_M08_US10_ST1.r &> 3_training_strategy_under_M08_US10_ST1.log &
+sleep 30m
+nohup Rscript 4.HT_lightgbm_under_M08_US10_ST1.r &> 4.HT_lightgbm_under_M08_US10_ST1.log &
