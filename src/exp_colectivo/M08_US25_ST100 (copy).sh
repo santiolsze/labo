64f3299d2bbe1109a0b1d9feec2786848464c624
santiolsze <solszevicki@gmail.com>
2022-10-26 23:13:50 -0300

    8meses15us

---
+++ src/exp_colectivo/M08_US25_ST100 (copy).sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+nohup Rscript 3_training_strategy_under_M08_US15_ST100.r &> 3_training_strategy_under_M08_US15_ST100.log &
+sleep 15m
+nohup Rscript 4.HT_lightgbm_under_M08_US15_ST100.r &> 4.HT_lightgbm_under_M08_US15_ST100.log &
