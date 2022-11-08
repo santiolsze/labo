2e25bd43414c05a180f68a00dd570e0ea2c2df12
santiolsze <solszevicki@gmail.com>
2022-11-08 15:21:46 -0300

    scripts c4v1 fe

--- src/competencia4/4_01/z932_training_strategy_under.r
+++ src/competencia4/4_01/z932_training_strategy_under.r
@@ -15,24 +15,26 @@ require("data.table")
 
 #Parametros del script
 PARAM  <- list()
-PARAM$experimento <- "TS9320"
+PARAM$experimento <- "TS401"
 
-PARAM$exp_input  <- "FE9250"
+PARAM$exp_input  <- "FE401"
 
-PARAM$future       <- c( 202107 )
+PARAM$future       <- c( 202109)
 
-PARAM$final_train  <- c( 202103, 202104, 202105 )
+PARAM$final_train  <- c(201906,201907,201908,201909,201910,201911,201912,
+                        202011,202012,202101,202102,202103,202104,202105,202106,202107)
 
-PARAM$train$training     <- c( 202101, 202102, 202103 )
-PARAM$train$validation   <- c( 202104 )
-PARAM$train$testing      <- c( 202105 )
+PARAM$train$training     <- c( 201904,201905,201906,201907,201908,201909,201910,201911,201912,
+                        202011,202012,202101,202102,202103,202104,202105)
+PARAM$train$validation   <- c( 202106)
+PARAM$train$testing      <- c( 202107)
 
 PARAM$train$sampling_total  <- 1.0  # 1.0 significa que NO se hace sampling total,  0.3 es quedarse con el 30% de TODOS los registros
-PARAM$train$undersampling_mayoritaria  <- 0.1   # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
+PARAM$train$undersampling_mayoritaria  <- 0.4   # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
 
 #Atencion, las semillas deben ser distintas
-PARAM$train$semilla_sampling  <- 102191
-PARAM$train$semilla_under     <- 892237
+#PARAM$train$semilla_sampling  <- 66667 NO SE USA
+PARAM$train$semilla_under     <- 66667
 # FIN Parametros del script
 
 
