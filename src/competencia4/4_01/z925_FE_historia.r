7ef86334429d1f36ee02e0b7d33219ee54e052e9
santiolsze <solszevicki@gmail.com>
2022-11-08 13:46:32 -0300

    scripts c4v1 fe

--- src/competencia4/4_01/z925_FE_historia.r
+++ src/competencia4/4_01/z925_FE_historia.r
@@ -19,14 +19,14 @@ require("lightgbm")
 
 #Parametros del script
 PARAM  <- list()
-PARAM$experimento <- "FE9250"
+PARAM$experimento <- "FE401"
 
-PARAM$exp_input  <- "DR9141"
+PARAM$exp_input  <- "DR401"
 
 PARAM$lag1  <- TRUE
-PARAM$lag2  <- FALSE
+PARAM$lag2  <- TRUE
 PARAM$Tendencias  <- TRUE
-PARAM$RandomForest  <- FALSE          #No se puede poner en TRUE para la entrega oficial de la Tercera Competencia
+PARAM$RandomForest  <- TRUE          #No se puede poner en TRUE para la entrega oficial de la Tercera Competencia
 PARAM$CanaritosAsesinos  <- FALSE
 # FIN Parametros del script
 
@@ -119,7 +119,7 @@ cppFunction('NumericVector fhistC(NumericVector pcolumna, IntegerVector pdesde )
 #la tendencia es la pendiente de la recta que ajusta por cuadrados minimos
 #La funcionalidad de ratioavg es autoria de  Daiana Sparta,  UAustral  2021
 
-TendenciaYmuchomas  <- function( dataset, cols, ventana=6, tendencia=TRUE, minimo=TRUE, maximo=TRUE, promedio=TRUE, 
+TendenciaYmuchomas  <- function( dataset, cols, ventana=3, tendencia=TRUE, minimo=TRUE, maximo=TRUE, promedio=TRUE, 
                                  ratioavg=FALSE, ratiomax=FALSE)
 {
   gc()
@@ -372,7 +372,7 @@ if( PARAM$Tendencias )
 {
   TendenciaYmuchomas( dataset, 
                       cols= cols_lagueables,
-                      ventana=   6,      # 6 meses de historia
+                      ventana=   3,      
                       tendencia= TRUE,
                       minimo=    FALSE,
                       maximo=    FALSE,
